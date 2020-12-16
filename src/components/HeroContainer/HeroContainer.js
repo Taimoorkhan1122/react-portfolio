1b52dbbd5b7d626a30b8250f14f221897b6139a5
Taimoor khan <khant8k83@gmail.com>
2020-12-16 18:40:45 +0500

    feat: hero section completed

--- src/components/HeroContainer/HeroContainer.js
+++ src/components/HeroContainer/HeroContainer.js
@@ -4,9 +4,18 @@ import styles from "./hero.module.css";
 const HeroContainer = () => {
   return (
     <div className={styles.heroContainer}>
-      <h1 className={styles.bigOne}>
-        Hey there <br /> Welcome
-      </h1>
+      <div className={styles.intro}>
+        <h1 className={styles.bigOne}>
+          Hey there <br /> Welcome
+        </h1>
+        <p>
+          My name is Taimoor khan I am Front-end Developer. I love to create
+          aesthetic UI with user centric approach. <br />
+          Thanks for visiting my website have a look at my portfolio and if you
+          have any querry you can contact me.
+        </p>
+      </div>
+      <img src="./images/header-image.svg" alt="developer illustration" />
     </div>
   );
 };
