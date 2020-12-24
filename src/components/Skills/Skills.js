0839b39e7b2021d22a9519dd9994db8aba77580e
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2020-12-24 16:50:39 +0500

    feat: skill section completed

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,22 +4,32 @@ import CreateSKills from "./CreateSKills";
 
 import styles from "./skills.module.css";
 const Skills = () => {
-  const skills = [
-    { name: "js", color: "yellow" },
+  const devSkills = [
+    { name: "js", color: `yellow` },
     { name: "react", color: "blue" },
     { name: "html5", color: "deepOrange" },
     { name: "css3", color: "blue" },
     { name: "git-alt", color: "orange" },
-    { name: "github", color: "orange" },
-    { name: "docker", color: "orange" },
+    { name: "github", color: "grey" },
+    { name: "docker", color: "blue" },
     { name: "kubernetes", color: "orange" },
     { name: "python", color: "orange" },
     { name: "rust", color: "brown" },
   ];
+
+  const DesignSkills = [
+    { name: "figma", color: `orange` },
+    { name: "xd", color: "blue" },
+    { name: "photoshop", color: "blue" },
+    { name: "ui", color: "blue" },
+  ];
   return (
-    <div>
+    <div className={styles.skillContainer}>
       <h2 className={styles.heading}>Development Skills</h2>
-      <CreateSKills skills={skills} />
+      <CreateSKills skills={devSkills} size="9vw" />
+      <br />
+      <h2 className={styles.heading}>Design Skills</h2>
+      <CreateSKills skills={DesignSkills} size="9vw" />
     </div>
   );
 };
